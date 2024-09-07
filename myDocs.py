6729bcd03f5a7420342ab7b01f7e68373a901d99
weebeewooboo <154526879+weebeewooboo@users.noreply.github.com>
2024-09-08 00:00:23 +0200

    Add files via upload

--- myDocs.py
+++ myDocs.py
@@ -913,6 +913,16 @@ make_pizza(30,'pieczarki','dodatkowy ser', 'ketchup')
 
 # używanie dowolnej liczby argumentów w postaci słów kluczowych
 
+def build_profile(first,last, **user_info):
+    '''Budowa słownika zawierającego wszelkie informacje o użytkowniku'''
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('albert', 'einstein', location = 'princeton',
+                             field = 'fizyka')
+
+print(user_profile)
 
 
 
